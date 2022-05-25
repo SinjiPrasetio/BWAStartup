a1e1b4470cec6c12c9ef66c4bf2aae47b7277a4a
Sinji Prasetio <sinjiprasetio@outlook.com>
2022-05-25 14:17:54 +0700

    first commit

---
+++ user/entity.go
@@ -0,0 +1,15 @@
+package user
+
+import "time"
+
+type User struct {
+	ID             int
+	Name           string
+	Occupation     string
+	Email          string
+	PasswordHash   string
+	AvatarFileName string
+	Role           string
+	UpdatedAt      time.Time
+	CreatedAt      time.Time
+}
